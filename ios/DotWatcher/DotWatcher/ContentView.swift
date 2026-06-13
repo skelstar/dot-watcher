dae88468842215715f58b610626a2d7068a2081b
skelstar <skelstar@gmail.com>
2026-06-13 12:14:54 +1200

    Created the iOS app. Just a Hello World at this stage.

---
+++ ios/DotWatcher/DotWatcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DotWatcher
+//
+//  Created by skelstar on 13/6/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/DotWatcher/DotWatcher/DotWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotWatcherApp.swift
+//  DotWatcher
+//
+//  Created by skelstar on 13/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct DotWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
