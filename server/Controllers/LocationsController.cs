7a973edd3ebfd9a6edab2a980a92874a05368b76
skelstar <skelstar@gmail.com>
2026-07-15 19:21:41 +1200

    Removed log fields and added runner to POST string

--- server/Controllers/LocationsController.cs
+++ server/Controllers/LocationsController.cs
@@ -41,7 +41,6 @@ public class LocationsController(
         store.AddPosition(validatedUpdate, user.UserId);
         HttpContext.Items["Log:Session"] = validatedUpdate.SessionId;
         HttpContext.Items["Log:Runner"] = validatedUpdate.RunnerName;
-        HttpContext.Items["Log:Heading"] = validatedUpdate.Heading.HasValue ? $"{validatedUpdate.Heading:F1}°" : "n/a";
         HttpContext.Items["Log:LocationTimestamp"] = validatedUpdate.Timestamp.ToString("HH:mm:ss");
         var participants = store.GetParticipants(validatedUpdate.SessionId);
         var positions = store.GetLatestPositions(validatedUpdate.SessionId);
